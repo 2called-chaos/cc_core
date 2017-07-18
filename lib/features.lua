65a472d63b8ca1fa365c206ed2b3f3b80ea8a499
chaos <chaos@CustoPro.local>
2017-07-18 18:57:44 +0200

    Huh what's going on

--- lib/features.lua
+++ lib/features.lua
@@ -1,6 +1,12 @@
 if ChaosCollection == nil then
+  print("setting up cc")
   ChaosCollection = setmetatable({}, ChaosCollection)
+else
+  print("WE HAVE CC?")
 end
 if ChaosCollection.Features == nil then
+  print("setting up features")
   ChaosCollection.Features = setmetatable({}, ChaosCollection)
+else
+  print("WE HAVE FEATURES?")
 end
