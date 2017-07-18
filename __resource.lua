d661920d30e49758c452176ade9e4b31b8b310e9
chaos <chaos@CustoPro.local>
2017-07-18 18:52:42 +0200

    Add feature tracking

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,9 @@
 -- Description
 description 'ChaosCollection: Core (by 2called-chaos) - Common functions'
 
+-- This is the only file we will load on startup to keep track of available CC-features
+server_scripts 'lib/features.lua'
+
 -- This file doesn't load any files, resources using this resource have to do that like this:
 
 -- server_scripts '@chacol_core/lib/utils.lua'
---
+++ lib/features.lua
@@ -0,0 +1,6 @@
+if ChaosCollection == nil then
+  ChaosCollection = setmetatable({}, ChaosCollection)
+end
+if ChaosCollection.Features == nil then
+  ChaosCollection.Features = setmetatable({}, ChaosCollection)
+end
